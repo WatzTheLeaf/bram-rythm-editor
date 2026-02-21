7647137a6a301750aadeee60a33d7e27ee81a362
WatzTheLeaf <louistheleaf@pm.me>
2026-02-21 10:38:17 +0100

    add export command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,13 @@
 use base64::{engine::general_purpose, Engine as _};
 use bram_audio_parser::load_presentation;
+use bram_input_exporter::export_inputs;
 
 #[tauri::command]
 fn get_wav_data(path: &str, rate: u32) -> Result<Vec<Vec<f32>>, String> {
     match load_presentation(path, rate) {
         Ok(presentation) => Ok(vec![
             presentation.left_channel_points,
-            presentation.right_channel_points
+            presentation.right_channel_points,
         ]),
 
         Err(e) => Err(e.to_string()),
@@ -20,6 +21,14 @@ fn read_audio_file_b64(path: &str) -> Result<String, String> {
     Ok(format!("data:audio/wav;base64,{}", base64))
 }
 
+#[tauri::command]
+fn export_barm_data(path: &str, data: Vec<u8>, rate: u8) -> Result<String, String> {
+    match export_inputs(path, &data, rate) {
+        Ok(_) => Ok("Export successful.".to_string()),
+        Err(e) => Err(format!("Error exporting barm: {}", e)),
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -27,7 +36,11 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_wav_data, read_audio_file_b64])
+        .invoke_handler(tauri::generate_handler![
+            get_wav_data,
+            read_audio_file_b64,
+            export_barm_data
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
